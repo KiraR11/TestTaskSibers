bb9f5fe78b8fa5ba1b77b239ca3202049f1b9ddf
KiraR11 <123009064+KiraR11@users.noreply.github.com>
2024-02-07 16:11:15 +0700

    функциокала сотрудника и проекта

---
+++ TestTastSibers_Model/Employee.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTaskSibers_Model
+{
+    public class Employee
+    {
+        public Employee(string name, string surname, string patronymic, string email, List<Project> projects)
+        {
+            Name = name;
+            Surname = surname;
+            Patronymic = patronymic;
+            Email = email;
+            Projects = projects;
+        }
+        private Employee(){}
+        private string _name;
+        private string _surname;
+        private string _patronymic;
+        private string _email;
+        public List<Project> Projects { get; set; }
+        private int Id { get; set; }
+        public string Name 
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+            set
+            {
+                _surname = value;
+            }
+        }
+        public string Patronymic
+        {
+            get
+            {
+                return _patronymic;
+            }
+            set
+            {
+                _patronymic = value;
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value;
+            }
+        }
+        public void AddProjects(Project project)
+        {
+            Projects.Add(project);
+            if(!project.Employees.Contains(this))
+                project.AddEmployee(this);
+        }
+        public void RemoveProjects(Project project)
+        {
+            Projects.Remove(project);
+            if (project.Employees.Contains(this))
+                project.RemoveEmployee(this);
+        }
+    }
+}
---
+++ TestTastSibers_Model/Project.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTaskSibers_Model
+{
+    public class Project
+    {
+        public Project(
+            string name,
+            string nameСustomerCompany,
+            string nameСontractorCompany,
+            DateOnly dataStart,
+            DateOnly dataEnd,
+            byte priority,
+            Employee director,
+            List<Employee> employees
+            ) {
+            Name = name;
+            NameСustomerCompany = nameСustomerCompany;
+            NameСontractorCompany = nameСontractorCompany;
+            DataStart = dataStart;
+            DataEnd = dataEnd;
+            Priority = priority;
+            Director = director;
+            Employees = employees;
+        }
+        private Project(){}
+
+        private string _name;
+        private string _nameСustomerCompany;
+        private string _nameСontractorCompany;
+        private DateOnly _dataStart;
+        private DateOnly _dataEnd;
+        private byte _priority;
+        private int Id { get; set; }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public string NameСustomerCompany
+        {
+            get
+            {
+                return _nameСustomerCompany;
+            }
+            set
+            {
+                _nameСustomerCompany = value;
+            }
+        }
+        public string NameСontractorCompany
+        {
+            get
+            {
+                return _nameСontractorCompany;
+            }
+            set
+            {
+                _nameСontractorCompany = value;
+            }
+        }
+        public byte Priority
+        {
+            get
+            {
+                return _priority;
+            }
+            set
+            {
+                _priority = value;
+            }
+        }
+        public DateOnly DataStart
+        {
+            get
+            {
+                return _dataStart;
+            }
+            set
+            {
+                _dataStart = value;
+            }
+        }
+        public DateOnly DataEnd
+        {
+            get
+            {
+                return _dataEnd;
+            }
+            set
+            {
+                _dataEnd = value;
+            }
+        }
+        public Employee Director { get; set; }
+        public List<Employee> Employees { get;}
+
+        public void AddEmployee(Employee employee)
+        {
+            Employees.Add(employee);
+            if(!employee.Projects.Contains(this))
+                employee.AddProjects(this);
+        }
+        public void RemoveEmployee(Employee employee)
+        {
+            Employees.Remove(employee);
+            if(employee.Projects.Contains(this))
+                employee.RemoveProjects(this);
+        }
+    }
+}
