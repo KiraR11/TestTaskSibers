c1d5985bcae9d91ff17687fca2ef4c7e5a20de3f
KiraR11 <123009064+KiraR11@users.noreply.github.com>
2024-02-07 14:18:49 +0700

    Добавьте файлы проекта.

---
+++ TestTaskSibers/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestTaskSibers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
