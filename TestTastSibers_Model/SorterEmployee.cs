f869bdb04775902bd576494339b301f845c45666
KiraR11 <123009064+KiraR11@users.noreply.github.com>
2024-02-07 16:16:21 +0700

    добавил классы сортировки

---
+++ TestTastSibers_Model/SorterEmployee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTaskSibers_Model
+{
+    public static class SorterEmployee
+    {
+    }
+}
